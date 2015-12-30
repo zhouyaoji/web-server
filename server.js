df5241eda4940d2bd73ebdc0e66c8e4ff63f90af
Joe Catera <zhouyaoji@yahoo.com>
2015-12-29 23:57:40 -0800

    Modified the server.js file.

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var middleware = {
 app.use(middleware.logger);
 
 app.get('/about', middleware.requireAuthentication, function(req, resp) {
-   resp.send('About Us'); 
+   resp.send('About Us!'); 
 });  
 app.use(express.static(__dirname + "/public"));
 
